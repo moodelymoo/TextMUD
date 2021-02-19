166bf7e003d2685882e14bf0b9807ced7c4a7a62
moodelymoo <moodelymoo@gmail.com>
2021-02-19 18:01:02 +0000

    Checking multiple file works as expected..

--- TextMUD/Program.cs
+++ TextMUD/Program.cs
@@ -2,11 +2,14 @@
 
 namespace TextMUD
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Runner runner = new Runner(1, "Frank");
+            runner.Diving();
+            Console.WriteLine(runner.ToString().Length);
         }
     }
 }
---
+++ TextMUD/Runner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TextMUD
+{
+    public class Runner
+    {
+        private int Dave { get; }
+        private string Name { get; }
+
+        public Runner(int dave, string name)
+        {
+            Dave = dave;
+            Name = name;
+        }
+        
+        public void Diving()
+        {
+            Console.WriteLine($"{Dave} Dave's, called {Name}");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
