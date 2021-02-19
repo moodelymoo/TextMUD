2fe9ae6e0b0b0c8070618f41d4654d8205f0a3a9
moodelymoo <moodelymoo@gmail.com>
2021-02-19 17:37:50 +0000

    Initial commit, testing what files are added, trying to make sure there are no IDE specific files that would break when converting between VS and Rider

---
+++ TextMUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextMUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
